7238d494a028a9f4063ff15847b187d94e4859f9
git-richard <richard@richardl.com>
2016-05-03 15:23:58 -0500

    Put component back in.

--- src/MyComp.jsx
+++ src/MyComp.jsx
@@ -2,6 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function Test() {
+  return (
+    <div>
+      test 2
+    </div>
+  );
+}
+
 // Use this syntax instead of extends React.Component for a stateless function.
 function MyComp() {
   return (
